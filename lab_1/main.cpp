e45172ac0cbe3902e2f604939bda0813c560cfa8
Marina <nozdryakovamarina94@gmail.com>
2025-02-11 22:51:38 +0400

    feat: method matrix multiplication feat: method load matrix from file feat: method record result

--- lab_1/main.cpp
+++ lab_1/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <random>
+#include <ctime>
 
 using namespace std;
 
@@ -50,10 +51,83 @@ void createMatrixFile(const string& filename, int rows, int cols, int minValue, 
 	saveMatrixToFile(filename, matrix);
 }
 
+vector<vector<int>> multiplyMatrix(vector<vector<int>> matrixA, vector<vector<int>> matrixB) {
+	if (matrixA.empty() || matrixB.empty()) {
+		throw runtime_error("Matrices must be filled in");
+	}
+	int rowsA = matrixA.size();
+	int colsA = matrixA[0].size();
+	int rowsB = matrixB.size();
+	int colsB = matrixB[0].size();
+
+	if (colsA != rowsB) {
+		throw runtime_error("It is impossible to multiply matrices: incompatible sizes");
+	}
+
+	vector<vector<int>> matrixxResult(rowsA, vector<int>(colsB));
+
+	for (int i = 0; i < rowsA; ++i) {
+		for (int j = 0; j < colsB; ++j) {
+			for (int k = 0; k < colsA; ++k) {
+				matrixxResult[i][j] += matrixA[i][k] * matrixB[k][j];
+			}
+		}
+	}
+
+	return matrixxResult;
+}
+
+vector<vector<int>> loadMatrixFromFile(const string& filename, int rows, int cols) {
+	ifstream input("/paralProg/lab_1/" + filename);
+
+
+	if (!input.is_open()) {
+		throw runtime_error("Couldn't open the file for writing: " + filename);
+	}
+
+	vector<vector<int>> matrix(rows, vector<int>(cols));
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			input >> matrix[i][j];
+		}
+	}
+
+	input.close();
+	return matrix;
+}
+
+void writeResult(string filename, vector<vector<int>>& matrixResult, int rows, int cols, clock_t end, clock_t start) { 
+
+	ofstream data("/paralProg/lab_1/" + filename);
+	if (!data.is_open()) {
+		throw runtime_error("Couldn't open the file for writing: " + filename);
+		return;
+	}
+
+	ofstream result("/paralProg/lab_1/result.txt", ios::app);
+	if (!result.is_open()) {
+		throw runtime_error("Couldn't open the file result.txt");
+		data.close();
+		return;
+	}
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			data << matrixResult[i][j] << " ";
+		}
+		data << endl;
+	}
+	data.close(); 
+
+	result << "Size matrix: " << rows << "x" << cols << " | " << "Lead time: " << (double)(end - start) / (double)(CLOCKS_PER_SEC) << endl;
+	result.close(); 
+}
+
 
 int main() {
-	string fileA;
-	int rowsA, colsA;
+	srand(time(nullptr));
+	string fileA, fileB, fileResult;
+	int rowsA, colsA, rowsB, colsB;
 	int minValue, maxValue;
 
 	cout << "Enter the file name for the first matrix:";
@@ -68,5 +142,31 @@ int main() {
 	cin >> maxValue;
 
 	createMatrixFile(fileA, rowsA, colsA, minValue, maxValue);
+
+	cout << "Enter the file name for the second matrix:";
+	cin >> fileB;
+
+	cout << "Enter the number of rows and columns for the second matrix:";
+	cin >> rowsB >> colsB;
+
+	createMatrixFile(fileB, rowsB, colsB, minValue, maxValue);
+
+	cout << "Enter the file name to save the result:";
+	cin >> fileResult; 
+
+	vector<vector<int>> matrixA, matrixB, resultMatrix;
+
+	matrixA = loadMatrixFromFile(fileA, rowsA, colsA);
+	matrixB = loadMatrixFromFile(fileB, rowsB, colsB);
+	 
+	clock_t start, end;
+	start = clock();
+
+	resultMatrix = multiplyMatrix(matrixA, matrixB);
+	
+	end = clock();
+
+	writeResult(fileResult, resultMatrix, resultMatrix.size(), resultMatrix[0].size(), end, start);
+
 	return 0;
 }
