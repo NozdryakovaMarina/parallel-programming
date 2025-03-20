75a5dd8433c2c2f9f93878b447c42db9464c3234
Marina <nozdryakovamarina94@gmail.com>
2025-03-20 14:11:18 +0400

    task 1 feat: add tech openMP feat: add cycle for multiple measurements of the same experiment

---
+++ lab_2/labOpenMP/labOpenMP.cpp
@@ -0,0 +1,189 @@
+﻿#include <omp.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+
+void saveMatrixToFile(const string& filename, vector<vector<int>>& matrix) {
+	ofstream out("/paralProg/lab_2/" + filename);
+
+	if (!out.is_open()) {
+		throw runtime_error("Couldn't open the file for writing: " + filename);
+	}
+	else
+	{
+		for (int i = 0; i < matrix.size(); ++i) {
+			for (int j = 0; j < matrix[0].size(); ++j) {
+				out << matrix[i][j] << " ";
+			}
+			out << endl;
+		}
+		out.close();
+		cout << "Successfully! Matrix is created and written to a file " << filename << endl;
+	}
+}
+
+vector<vector<int>> createMatrix(int rows, int cols, int minValue, int maxValue) {
+	if (rows <= 0 || cols <= 0) {
+		throw runtime_error("Size of the matrix must be positive");
+	}
+
+	vector<vector<int>> matrix(rows, vector<int>(cols));
+	random_device rd;
+	mt19937 generator(rd());
+	uniform_int_distribution<> distrib(minValue, maxValue);
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			matrix[i][j] = distrib(generator);
+		}
+	}
+
+	return matrix;
+}
+
+
+void createMatrixFile(const string& filename, int rows, int cols, int minValue, int maxValue) {
+	vector<vector<int>> matrix = createMatrix(rows, cols, minValue, maxValue);
+	saveMatrixToFile(filename, matrix);
+}
+
+vector<vector<int>> multiplyMatrix(vector<vector<int>> matrixA, vector<vector<int>> matrixB) {
+	if (matrixA.empty() || matrixB.empty()) {
+		throw runtime_error("Matrices must be filled in");
+	}
+	int rowsA = matrixA.size();
+	int colsA = matrixA[0].size();
+	int rowsB = matrixB.size();
+	int colsB = matrixB[0].size();
+
+	if (colsA != rowsB) {
+		throw runtime_error("It is impossible to multiply matrices: incompatible sizes");
+	}
+
+	vector<vector<int>> matrixxResult(rowsA, vector<int>(colsB));
+
+    #pragma omp parallel for 
+	for (int i = 0; i < rowsA; ++i) {
+		for (int j = 0; j < colsB; ++j) {
+			int sum = 0;
+            #pragma omp parallel for reduction(+:sum) 
+			for (int k = 0; k < colsA; ++k) {
+				sum += matrixA[i][k] * matrixB[k][j];
+			}
+			matrixxResult[i][j] = sum;
+		}
+	}
+
+	return matrixxResult;
+}
+
+vector<vector<int>> loadMatrixFromFile(const string& filename, int rows, int cols) {
+	ifstream input("/paralProg/lab_2/" + filename);
+
+
+	if (!input.is_open()) {
+		throw runtime_error("Couldn't open the file for writing: " + filename);
+	}
+
+	vector<vector<int>> matrix(rows, vector<int>(cols));
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			input >> matrix[i][j];
+		}
+	}
+
+	input.close();
+	return matrix;
+}
+
+void writeResult(string filename, vector<vector<int>>& matrixResult, int rows, int cols, double averageTime) {
+
+	ofstream data("/paralProg/lab_2/" + filename);
+	if (!data.is_open()) {
+		throw runtime_error("Couldn't open the file for writing: " + filename);
+		return;
+	}
+
+	ofstream result("/paralProg/lab_2/result.txt", ios::app);
+	if (!result.is_open()) {
+		throw runtime_error("Couldn't open the file result.txt");
+		data.close();
+		return;
+	}
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			data << matrixResult[i][j] << " ";
+		}
+		data << endl;
+	}
+	data.close();
+
+	result << "Size matrix: " << rows << "x" << cols << " | " << "Average lead time: " << averageTime << " seconds" << endl;
+	result.close();
+}
+
+
+int main() {
+
+	srand(time(nullptr));
+	string fileA, fileB, fileResult;
+	int rowsA, colsA, rowsB, colsB;
+	int minValue, maxValue;
+
+	//cout << omp_get_max_threads();
+	omp_set_num_threads(20);
+
+	cout << "Enter the file name for the first matrix:";
+	cin >> fileA;
+
+	cout << "Enter the number of rows and columns for the first matrix:";
+	cin >> rowsA >> colsA;
+
+	cout << "Enter the minimum value for the matrix elements: ";
+	cin >> minValue;
+	cout << "Enter the maximum value for the matrix elements: ";
+	cin >> maxValue;
+
+	createMatrixFile(fileA, rowsA, colsA, minValue, maxValue);
+
+	cout << "Enter the file name for the second matrix:";
+	cin >> fileB;
+
+	cout << "Enter the number of rows and columns for the second matrix:";
+	cin >> rowsB >> colsB;
+
+	createMatrixFile(fileB, rowsB, colsB, minValue, maxValue);
+
+	cout << "Enter the file name to save the result:";
+	cin >> fileResult;
+
+	vector<vector<int>> matrixA, matrixB, resultMatrix;
+
+	matrixA = loadMatrixFromFile(fileA, rowsA, colsA);
+	matrixB = loadMatrixFromFile(fileB, rowsB, colsB);
+
+	const int numExperiments = 10;
+	double totalTime = 0.0;
+
+	for (int i = 0; i < numExperiments; ++i) {
+		clock_t start = clock();
+
+		resultMatrix = multiplyMatrix(matrixA, matrixB);
+
+		clock_t end = clock();
+		double timeTaken = (double)(end - start) / CLOCKS_PER_SEC;
+		totalTime += timeTaken;
+	}
+
+	double averageTime = totalTime / numExperiments;
+
+	writeResult(fileResult, resultMatrix, resultMatrix.size(), resultMatrix[0].size(), averageTime);
+
+	return 0;
+}
